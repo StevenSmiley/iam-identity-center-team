d378187dffc827b4cc9f596e9d3c6352ad5d7780
Steven Smiley <smiley@scalesec.com>
2023-08-07 13:48:14 -0700

    case on status

--- amplify/backend/function/teamNotifySlack/src/index.py
+++ amplify/backend/function/teamNotifySlack/src/index.py
@@ -7,7 +7,7 @@ from slack_sdk.errors import SlackApiError
 import os
 import boto3
 from datetime import datetime
-from dateutil import tz
+from dateutil import parser, tz
 
 def get_settings():
     response = settings_table.get_item(
@@ -30,45 +30,71 @@ except Exception as error:
     exit
 
 
-def lambda_handler(event, context):
+def lambda_handler(event: dict, context):
+    request_status = event["status"]
+    requester_email = event["email"]
+    approvers = event.get("approvers", "")
+    account = "{0} ({})".format(event["accountName"], event["accountId"])
+    role = event["role"]
+    # TODO: Make duration more readable
+    duration = event["duration"]
+    justification = event.get("justification", "No justification provided")
+    ticket = event.get("ticketNo", "No ticket provided")
+    # TODO: Get the login url from somewhere
+    login_url = "http://aws.team"
+
+    match request_status:
+        case "pending":
+            # Notify approvers pending request
+            pass
+        case "expired":
+            # Notify requester request expired
+            pass
+        case "rejected":
+            # Notify requester request rejected
+            pass
+        case "cancelled":
+            # Notify approvers request cancelled
+            pass
+        case "scheduled":
+            # Notify requester access scheduled
+            pass
+        case "in progress":
+            # Notify requester access granted
+            pass
+        case "error":
+            # Notify approvers and requester error
+            pass
+        case "ended" | "revoked":
+            # Notify requester ended
+            pass
+        case _:
+            print(f"Request status unexpected: {request_status}")
+
     # TODO: loop through people as applicable
 
     # Get Slack user ids for each person, by their email address
-    user_email = event["email"]
-    
     try:
-        user = slack_client.users_lookupByEmail(email=user_email)
-        user_id = user["user"]["id"]
+        requester = slack_client.users_lookupByEmail(email=requester_email)
+        requester_slack_id = requester["user"]["id"]
         approver_timezone = timezone = tz.gettz(name=["user"]["tz"])
     except SlackApiError as error:
-        print("Error getting Slack user info for {0}: {1}".format(user_email, error))
+        print("Error getting Slack user info for {0}: {1}".format(requester_email, error))
         return
     
     # Build formatted date, localized to approver's timezone
-    utc_zone=tz.tzutc()
-    # TODO: populate this from the request data
-    request_start_time = "2023-04-21T12:43:39.879Z"
-    parsed_date = datetime.strptime(request_start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
-    parsed_date = parsed_date.replace(tzinfo=utc_zone)
+    request_start_time = event["startTime"]
+    parsed_date = parser.parse(request_start_time)
     localized_date = parsed_date.astimezone(approver_timezone)
     formatted_date = localized_date.strftime('%B %d, %Y at %I:%M %p %Z')
 
     # Build message using Slack blocks
-    # TODO: populate these from the request data
-    status = "pending"
-    requester = "requester@domain.com"
-    account = "Testing (123456789012)"
-    role = "CICDAdministrator"
-    duration = "1 hours"
-    justification = "Need access to fix the CICD pipeline"
-    ticket = "JIRA111"
-    login_url = "http://aws.team"
     message_blocks = [
         {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": "AWS Access Request from {0} is *{1}*".format(requester, status),
+                "text": "AWS Access Request from {0} is *{1}*".format(requester_email, request_status),
             },
         },
         {
@@ -106,14 +132,14 @@ def lambda_handler(event, context):
     # Send message to user
     try:
         post_message_response = slack_client.chat_postMessage(
-            channel=user_id,
+            channel=requester_slack_id,
             blocks=message_blocks,
             text="AWS Access Request Notification",
         )
     except SlackApiError as error:
         print(
             "Error posting chat message to channel/user id {0}: {1}".format(
-                user_id, error
+                requester_slack_id, error
             )
         )
 
