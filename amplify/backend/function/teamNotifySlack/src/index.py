ca8594bd5f4c4743585ce87165a8b06c17faa533
Steven Smiley <smiley@scalesec.com>
2023-08-08 17:31:54 -0700

    cleanup

--- amplify/backend/function/teamNotifySlack/src/index.py
+++ amplify/backend/function/teamNotifySlack/src/index.py
@@ -3,22 +3,107 @@
 # http: // aws.amazon.com/agreement or other written agreement between Customer and either
 # Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 import os
 import boto3
 from dateutil import parser, tz
 
-dynamodb = boto3.resource("dynamodb")
-settings_table_name = os.getenv("SETTINGS_TABLE_NAME")
-settings_table = dynamodb.Table(settings_table_name)
-settings = settings_table.get_item(Key={"id": "settings"})
-item_settings = settings.get("Item", {})
+
 try:
+    dynamodb = boto3.resource("dynamodb")
+    settings_table_name = os.getenv("SETTINGS_TABLE_NAME")
+    settings_table = dynamodb.Table(settings_table_name)
+    settings = settings_table.get_item(Key={"id": "settings"})
+    item_settings = settings.get("Item", {})
     slack_token = item_settings.get("slackToken")
     slack_client = WebClient(token=slack_token)
 except Exception as error:
     print(f"Error retrieving Slack OAuth token, cannot continue: {error}")
     exit
+
+
+def send_slack_notifications(
+    recipients: list,
+    message,
+    login_url,
+    request_start_time,
+    role,
+    account,
+    duration_hours,
+    justification="",
+    ticket="",
+):
+    parsed_date = parser.parse(request_start_time)
+
+    for recipient in recipients:
+        try:
+            recipient_slack_user = slack_client.users_lookupByEmail(email=recipient)
+            recipient_slack_id = recipient_slack_user["user"]["id"]
+            recipient_timezone = tz.gettz(name=recipient_slack_user["user"]["tz"])
+        except Exception as error:
+            print(f"Error getting Slack user info for {recipient}: {error}")
+            continue
+
+        # Format date, localized to recipient's timezone
+        localized_date = parsed_date.astimezone(recipient_timezone)
+        formatted_date = localized_date.strftime("%B %d, %Y at %I:%M %p %Z")
+
+        # Build message using Slack blocks
+        message_blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*{message}*",
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Open TEAM",
+                    },
+                    "url": login_url,
+                    "action_id": "button-action",
+                },
+            },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Account:*\n{account}",
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Start time:*\n{formatted_date}",
+                    },
+                    {"type": "mrkdwn", "text": f"*Role:*\n{role}"},
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Duration:*\n{duration_hours} hours",
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Justification:*\n{justification}",
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Ticket Number:*\n{ticket}",
+                    },
+                ],
+            },
+        ]
+
+        # Send message to user
+        try:
+            slack_client.chat_postMessage(
+                channel=recipient_slack_id,
+                blocks=message_blocks,
+                text="AWS Access Request Notification",
+            )
+        except Exception as error:
+            print(
+                f"Error posting chat message to channel/user id {recipient_slack_id}: {error}"
+            )
 
 
 def lambda_handler(event: dict, context):
@@ -140,88 +225,3 @@ def lambda_handler(event: dict, context):
             )
         case _:
             print(f"Request status unexpected: {request_status}")
-
-
-def send_slack_notifications(
-    recipients: list,
-    message,
-    login_url,
-    request_start_time,
-    role,
-    account,
-    duration_hours,
-    justification="",
-    ticket="",
-):
-    parsed_date = parser.parse(request_start_time)
-
-    for recipient in recipients:
-        try:
-            recipient_slack_user = slack_client.users_lookupByEmail(email=recipient)
-            recipient_slack_id = recipient_slack_user["user"]["id"]
-            recipient_timezone = tz.gettz(name=recipient_slack_user["user"]["tz"])
-        except SlackApiError as error:
-            print(f"Error getting Slack user info for {recipient}: {error}")
-            continue
-
-        # Format date, localized to recipient's timezone
-        localized_date = parsed_date.astimezone(recipient_timezone)
-        formatted_date = localized_date.strftime("%B %d, %Y at %I:%M %p %Z")
-
-        # Build message using Slack blocks
-        message_blocks = [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"*{message}*",
-                },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Open TEAM",
-                    },
-                    "url": login_url,
-                    "action_id": "button-action",
-                },
-            },
-            {
-                "type": "section",
-                "fields": [
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Account:*\n{account}",
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Start time:*\n{formatted_date}",
-                    },
-                    {"type": "mrkdwn", "text": f"*Role:*\n{role}"},
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Duration:*\n{duration_hours} hours",
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Justification:*\n{justification}",
-                    },
-                    {
-                        "type": "mrkdwn",
-                        "text": f"*Ticket Number:*\n{ticket}",
-                    },
-                ],
-            },
-        ]
-
-        # Send message to user
-        try:
-            post_message_response = slack_client.chat_postMessage(
-                channel=recipient_slack_id,
-                blocks=message_blocks,
-                text="AWS Access Request Notification",
-            )
-        except SlackApiError as error:
-            print(
-                f"Error posting chat message to channel/user id {recipient_slack_id}: {error}"
-            )
